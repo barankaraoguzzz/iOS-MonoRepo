03412b452fae3e6b13a4e1fa4e9108dc3282a03e
Baran Karaoğuz <b.b.karaoguz@gmail.com>
2025-07-18 10:52:35 +0300

    feat: Added first shareable module.
    * Added first shareable module
    * Creating simple MVVM Architecture
    * Integrated shareable module on two apps

---
+++ Packages/Core/NetworkModule/Sources/NetworkModule/NetworkModule.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class NetworkModuleManager {
+    
+    static public func fetchData() async {
+        
+    }
+    
+}
